1a8d150c8d29ff2698ed5e5eb93116692cbb0fe4
gardneli <lelandgardner@gmail.com>
2016-09-03 20:07:26 -0700

    First commit.

---
+++ com.google.testsbt/src/Main.scala
@@ -0,0 +1,29 @@
+/**
+  * Created by Lee on 9/3/2016.
+  */
+
+object Main extends App {
+  println("Testing lines and vectors!")
+  //val v1 = new Vector(List(1.0, 2, 3.0))
+  //val v2 = new Vector(List(3, 4, 5))
+  //println(v1 * v2)
+
+  val l1 = new Line(List(4.046, 2.836), 1.21)
+  val l2 = new Line(List(10.115, 7.09), 3.025)
+
+  val l3 = new Line(List(7.204, 3.182), 8.68)
+  val l4 = new Line(List(8.172, 4.114), 9.883)
+
+  val l5 = new Line(List(1.182, 5.562), 6.744)
+  val l6 = new Line(List(1.773, 8.343), 9.525)
+
+  println("Intersection: " + l1.intersectionPointWithLine(l2))
+  println("Lines are parallel: " + l1.isLineParallel(l2))
+  println("Lines are equal: " + l1.equals(l2))
+  println("Intersection: " + l3.intersectionPointWithLine(l4))
+  println("Lines are parallel: " + l3.isLineParallel(l4))
+  println("Intersection: " + l5.intersectionPointWithLine(l6))
+  println("Lines are parallel: " + l5.isLineParallel(l6))
+  println("Lines are equal: " + l5.equals(l6))
+
+}
